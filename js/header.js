adbb36be5f3b1ae635f8446a5e168de23d3bffd1
Jam Batista <batistadado@gmail.com>
2025-02-03 15:01:38 -0300

    up

--- js/header.js
+++ js/header.js
@@ -4,13 +4,24 @@ fetch("../support/header.html")
   .then((data) => {
     document.querySelector("header").innerHTML = data; // Insere o conteúdo do header
 
-    // Após o carregamento do header, anexe o evento de clique ao menu
+    // Seleciona os elementos após a inserção do header
     const menuToggle = document.querySelector(".menu-toggle");
     const menu = document.querySelector("ul.menu");
 
     if (menuToggle && menu) {
-      menuToggle.addEventListener("click", () => {
-        menu.classList.toggle("active"); // Exibe ou esconde o menu
+      // Evento para abrir e fechar o menu
+      menuToggle.addEventListener("click", (event) => {
+        menu.classList.toggle("active");
+
+        // Impede que o clique no botão feche imediatamente o menu
+        event.stopPropagation();
+      });
+
+      // Evento para fechar o menu ao clicar fora dele
+      document.addEventListener("click", (event) => {
+        if (!menu.contains(event.target) && !menuToggle.contains(event.target)) {
+          menu.classList.remove("active");
+        }
       });
     }
   })
